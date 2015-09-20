d1aa9ddd7fa3d46eee4e782d8551609cbb24c04c
Nicolas McCurdy <thenickperson@gmail.com>
2015-09-19 20:36:07 -0400

    Move example files to a subdirectory

--- example/api.js
+++ example/api.js
@@ -1,5 +1,5 @@
 var add = require('./add');
-var final = require('.');
+var final = require('..');
 var http = require('http');
 
 http.createServer(final.createServer(add)).listen(3000);
--- example/cli.js
+++ example/cli.js
@@ -1,4 +1,4 @@
 var add = require('./add');
-var final = require('.');
+var final = require('..');
 
 final.createCommand(add)();
