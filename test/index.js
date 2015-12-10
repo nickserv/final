d83b1003c33386c30289d54610df2456bb14ffc3
Nicolas McCurdy <thenickperson@gmail.com>
2015-12-10 00:27:57 -0500

    Fix spec nesting

--- test/index.js
+++ test/index.js
@@ -2,74 +2,76 @@ require('./helper')
 var http = require('http')
 var sinon = require('sinon')
 
-describe('final.CLI', () => {
-  var cli
+describe('final', () => {
+  describe('CLI', () => {
+    var cli
 
-  beforeEach(() => cli = new final.CLI(add))
+    beforeEach(() => cli = new final.CLI(add))
 
-  describe('constructor', () => {
-    it('creates a new CLI with the given core', () => {
-      assert.equal(cli.core, add)
+    describe('constructor', () => {
+      it('creates a new CLI with the given core', () => {
+        assert.equal(cli.core, add)
+      })
+    })
+
+    describe('#run()', () => {
+      it('runs a cli for the given core', sinon.test(function () {
+        this.stub(console, 'log')
+        process.argv = 'node cli.js -x 1 -y 2'.split(' ')
+        cli.run()
+
+        sinon.assert.calledOnce(console.log)
+        sinon.assert.calledWithExactly(console.log, 3)
+      }))
     })
   })
 
-  describe('#run()', () => {
-    it('runs a cli for the given core', sinon.test(function () {
-      this.stub(console, 'log')
-      process.argv = 'node cli.js -x 1 -y 2'.split(' ')
-      cli.run()
+  describe('Server', () => {
+    var server
 
-      sinon.assert.calledOnce(console.log)
-      sinon.assert.calledWithExactly(console.log, 3)
-    }))
-  })
-})
+    beforeEach(() => server = new final.Server(add))
 
-describe('final.Server', () => {
-  var server
+    afterEach(() => server.close())
 
-  beforeEach(() => server = new final.Server(add))
+    describe('constructor', () => {
+      it('creates a new Server with the given core', () => {
+        assert.equal(server.core, add)
+      })
 
-  afterEach(() => server.close())
+      it('creates a new Server with a callback', () => {
+        assert(server.callback instanceof Function)
+        assert.equal(server.callback.length, 2)
+      })
 
-  describe('constructor', () => {
-    it('creates a new Server with the given core', () => {
-      assert.equal(server.core, add)
+      it('creates a new Server with a server', () => {
+        assert(server.server instanceof http.Server)
+      })
     })
 
-    it('creates a new Server with a callback', () => {
-      assert(server.callback instanceof Function)
-      assert.equal(server.callback.length, 2)
+    describe('#close()', () => {
+      it('closes the Server', (done) => {
+        server.close()
+
+        http.get('http://localhost:3000', res => {
+          done('Error: Server should be closed')
+        }).on('error', () => done())
+      })
     })
 
-    it('creates a new Server with a server', () => {
-      assert(server.server instanceof http.Server)
-    })
-  })
+    describe('#run()', () => {
+      it('runs a Server for the given core', (done) => {
+        server.run()
 
-  describe('#close()', () => {
-    it('closes the Server', (done) => {
-      server.close()
+        http.get('http://localhost:3000?x=1&y=2', res => {
+          assert.equal(res.statusCode, 200)
+          assert.equal(res.headers['content-type'], 'text/plain')
 
-      http.get('http://localhost:3000', res => {
-        done('Error: Server should be closed')
-      }).on('error', () => done())
-    })
-  })
-
-  describe('#run()', () => {
-    it('runs a Server for the given core', (done) => {
-      server.run()
-
-      http.get('http://localhost:3000?x=1&y=2', res => {
-        assert.equal(res.statusCode, 200)
-        assert.equal(res.headers['content-type'], 'text/plain')
-
-        res.on('data', (chunk) => {
-          assert.equal(chunk.toString('utf8'), '3\n')
-          done()
-        })
-      }).on('error', done)
+          res.on('data', (chunk) => {
+            assert.equal(chunk.toString('utf8'), '3\n')
+            done()
+          })
+        }).on('error', done)
+      })
     })
   })
 })
