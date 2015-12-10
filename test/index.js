8dd37037a5db4add2c8c29636f74301c281f1770
Nicolas McCurdy <thenickperson@gmail.com>
2015-12-10 00:26:50 -0500

    Move test sources to a single file

--- test/index.js
+++ test/index.js
@@ -1,5 +1,29 @@
 require('./helper')
 var http = require('http')
+var sinon = require('sinon')
+
+describe('final.CLI', () => {
+  var cli
+
+  beforeEach(() => cli = new final.CLI(add))
+
+  describe('constructor', () => {
+    it('creates a new CLI with the given core', () => {
+      assert.equal(cli.core, add)
+    })
+  })
+
+  describe('#run()', () => {
+    it('runs a cli for the given core', sinon.test(function () {
+      this.stub(console, 'log')
+      process.argv = 'node cli.js -x 1 -y 2'.split(' ')
+      cli.run()
+
+      sinon.assert.calledOnce(console.log)
+      sinon.assert.calledWithExactly(console.log, 3)
+    }))
+  })
+})
 
 describe('final.Server', () => {
   var server
